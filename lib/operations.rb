35e49efcf511c5492a82f74a83ebcb17034da733
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-03 16:50:11 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+speed > 60
+	true
+	speed.between?(40,60) ? false : true
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60
+		true
+		speed.between?(40,60) ? false : true
 end
-	
-
-
